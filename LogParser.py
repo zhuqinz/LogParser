da1bc7a9c636b68fd0dfea9cbe45e50afa1f22ec
zhuqinz <zhuqinz@users.noreply.github.com>
2018-01-02 22:27:27 +1300

    Update log parser tool, auto refresh not implemented.

---
+++ LogParser.py
@@ -0,0 +1,123 @@
+import ntpath
+import os
+import threading
+
+
+try:
+    from Tkinter import *
+    import tkFont
+    import ttk
+except ImportError:  # Python 3
+    from tkinter import *
+    import tkinter.font as tkFont
+    import tkinter.ttk as ttk
+
+table_header = ['File Name', 'Caught', 'Full line']
+
+     
+class LogParser(threading.Thread):
+
+    def __init__(self, path, watchList, tabControl):
+        threading.Thread.__init__(self)
+        self.tree = None
+        self.caught_list = []
+        
+        self.watchList = watchList
+        self.path = path
+        self.tabControl = tabControl
+        self.tab = None
+        
+        self._setup_widgets()
+        self._read_log()
+        self._build_tree()
+
+    def _setup_widgets(self):
+        
+        # New Tab
+        self.tab = ttk.Frame(self.tabControl)
+        tabName = ntpath.basename(self.path)
+        if (tabName == ''):
+            tabName = 'Main'
+        self.tabControl.add(self.tab, text=tabName)
+        self.tabControl.pack(expand=1, fill="both", side=BOTTOM, ipadx=5, ipady=5, padx=5, pady=5)
+        # New Window
+#         self.root = Tk()
+#         container = ttk.Frame(self.root)
+#         container.pack(fill='both', expand=True)
+
+        self.tree = ttk.Treeview(columns=table_header, show="headings")
+        vsb = ttk.Scrollbar(orient="vertical",
+            command=self.tree.yview)
+        hsb = ttk.Scrollbar(orient="horizontal",
+            command=self.tree.xview)
+        self.tree.configure(yscrollcommand=vsb.set,
+            xscrollcommand=hsb.set)
+        self.tree.grid(column=0, row=0, sticky='nsew', in_=self.tab, padx=5, pady=5)
+        vsb.grid(column=1, row=0, sticky='ns', in_=self.tab)
+        hsb.grid(column=0, row=1, sticky='ew', in_=self.tab)
+        self.tab.grid_columnconfigure(0, weight=1)
+        self.tab.grid_rowconfigure(0, weight=1)
+
+    def _read_log(self):
+        files = [f for f in os.listdir(self.path) if re.match('.*.log', f)]
+         
+        for f in files:
+            p = os.path.join(self.path, f)
+            try:
+                with open(p, 'r') as logFile:
+                    for line in logFile:
+                        if any(s in line for s in self.watchList):
+                            self.recordFinding(ntpath.basename(p), line)
+            except IOError, err:
+                    print("Cannot open file '%s'." % p.name)
+         
+    def recordFinding(self, fileName, line):
+        match = ''
+        for l in self.watchList:
+            if l in line:
+                match = l
+        row = (fileName, match, line.strip())
+        self.caught_list.append(row)
+    
+#     def displayData(self):
+#         if(self.autoRefresh.get()==0):
+#             print "No auto refresh"
+#             self._read_log()
+#             self._build_tree()
+#         else:
+#             print "Auto Refreshing..."
+#             self._read_log()
+#             self._build_tree()
+#             self.tab.after(2000, self.displayData())
+
+    def _build_tree(self):
+        for col in table_header:
+            self.tree.heading(col, text=col.title(),
+                command=lambda c=col: sortby(self.tree, c, 0))
+            # adjust the column's width to the header string
+            self.tree.column(col,
+                width=tkFont.Font().measure(col.title()))
+
+        for item in self.caught_list:
+            self.tree.insert('', 'end', values=item)
+            # adjust column's width if necessary to fit each value
+            for ix, val in enumerate(item):
+                col_w = tkFont.Font().measure(val)
+                if ((self.tree.column(table_header[ix], width=None) < col_w) & (ix != 2)):
+                    self.tree.column(table_header[ix], width=col_w)
+    
+def sortby(tree, col, descending):
+    """sort tree contents when a column header is clicked on"""
+    # grab values to sort
+    data = [(tree.set(child, col), child) \
+        for child in tree.get_children('')]
+    # if the data to be sorted is numeric change to float
+    # data =  change_numeric(data)
+    # now sort the data in place
+    data.sort(reverse=descending)
+    for ix, item in enumerate(data):
+        tree.move(item[1], '', ix)
+    # switch the heading so it will sort in the opposite direction
+    tree.heading(col, command=lambda col=col: sortby(tree, col, \
+        int(not descending)))
+
--- LogParserOverview.py
+++ LogParserOverview.py
@@ -1,17 +1,26 @@
 '''
-LogParser is used to 
+LogParserOverview is used to 
 - read all the log files under base directory (baseDir)
 - read all the log files under sub-directories of base directory
-- Each directory will be treated as one unit, related info will be displayed in related tabs
-- if any content in the log files match the (error) content listed in watchList
-  display in the table, and save it to xxxx_error.log (xxx is original log file name)
+- Each directory will be treated as one target, related info will be displayed in related tabs (_LogParser Class)
 - It supports to analyze log files lively (auto refresh) 
 
 Author: Zhuqin
 '''
-from Tkinter import *
+import ntpath
 import os
 import tkFileDialog
+import LogParser
+
+
+try:
+    from Tkinter import *
+    import ttk
+except ImportError:  # Python 3
+    from tkinter import *
+    import tkinter.ttk as ttk
+
+table_header = ['File Name', 'Caught', 'Full line']
 
 
 class LogParserOverview (object):
@@ -20,29 +29,179 @@ class LogParserOverview (object):
         self.master = master
         self.title = title
         self.baseDir = StringVar()
-        self.watchList = ''
-        
+        self.watchListName = StringVar()
+        self.watchList = []
+        self.autoRefresh = IntVar()
+        self.jobs = []
         self.OnInit()
         
     def OnInit(self):
         # Create Main Window
-        self.master.title(self.title)
+        self.master.wm_title(self.title)
         self.master.geometry('1100x700')
         self.master.resizable(0, 1)
         
-        # Configuration frame for user to select files
-        mainFrame = LabelFrame(master=self.master, text='Configuration ', height='300', labelanchor='nw')
-        mainFrame.pack(anchor=N, fill=X, expand=True, padx=5)
+        # ===Configuration frame for user to select files===
+        configFrame = LabelFrame(self.master, text='Configuration ', height='300')
+        configFrame.grid(row=0, columnspan=4, sticky='WE', padx=5, pady=5, ipadx=5, ipady=5)
         # Log file location base directory
-        baseDir_lbl = Label(mainFrame, text='Log Files Base Directory:')
-        baseDir_lbl.pack(anchor=NW, side=LEFT, padx=5, pady=5)
-        baseDir_entry = Entry(mainFrame, relief=FLAT, width=80, textvariable=self.baseDir)
-        baseDir_entry.pack(padx=5, pady=5, side=LEFT, expand=True)
-        baseDir_btn = Button(mainFrame, text='Browse', command=self.loadBaseDirectory)
-        baseDir_btn.pack(side=RIGHT, padx=5, pady=5, expand=True)
-            
+        baseDir_lbl = Label(configFrame, text='Log Files Base Directory:')
+        baseDir_lbl.grid(row=0, column=0, sticky=W, padx=5, pady=2)
+        baseDir_txt = Entry(configFrame, textvariable=self.baseDir, width=80)
+        baseDir_txt.grid(row=0, column=1, columnspan=2, sticky=NW, pady=3)
+        baseDir_btn = Button(configFrame, text='Browse...', command=self.loadBaseDirectory)
+        baseDir_btn.grid(row=0, column=7, sticky=E, padx=5, pady=2)
+        # Watch list file location
+        baseDir_lbl = Label(configFrame, text='Watch List File: ')
+        baseDir_lbl.grid(row=1, column=0, sticky=W, padx=5, pady=2)
+        baseDir_txt = Entry(configFrame, textvariable=self.watchListName, width=80)
+        baseDir_txt.grid(row=1, column=1, columnspan=5, sticky=NW, pady=3)
+        baseDir_btn = Button(configFrame, text='Browse...', command=self.loadWatchList)
+        baseDir_btn.grid(row=1, column=7, sticky=E, padx=5, pady=2)
+                
+        # ===Control frame===
+        # Auto refresh
+        controlFrame = LabelFrame(self.master, text='Control ', height='300')
+        controlFrame.grid(row=1, columnspan=7, sticky='WE', padx=5, pady=5, ipadx=5, ipady=5)
+        autoRefresh_ck = Checkbutton(controlFrame, text='Auto Refresh', variable=self.autoRefresh)
+        autoRefresh_ck.grid(row=0, column=0, sticky=W , padx=15, pady=2)
+        start_btn = Button(controlFrame, text='Start', command=self.onClickStart, width=10)
+        start_btn.grid(row=0, column=1, sticky=E, padx=5, pady=2)
+        blank_lbl_2 = Label(controlFrame, width=20)
+        blank_lbl_2.grid(row=0, column=2, sticky=W, padx=5, pady=2)
+        stop_btn = Button(controlFrame, text='Stop', command=self.onClickStop, width=10)
+        stop_btn.grid(row=0, column=3, sticky=E, padx=5, pady=2)
+        blank_lbl_3 = Label(controlFrame, width=20)
+        blank_lbl_3.grid(row=0, column=4, sticky=W, padx=5, pady=2)
+        exit_btn = Button(controlFrame, text='Exit', command=self.onClickExit, width=10)
+        exit_btn.grid(row=0, column=5, sticky=E, padx=5, pady=2)
+        
+        # ===== Result screen display frame ====
+        self.resultFrame = LabelFrame(self.master, text='Result Screen')
+        self.resultFrame.grid(row=2, columnspan=7, sticky='WE', padx=5, pady=5, ipadx=5, ipady=5)
+        s = 'Analyzed result in log file'
+        msg = Message(self.resultFrame, text=s, relief=SUNKEN, width=400)
+        msg.pack(side=TOP)
+        self.tabControl = ttk.Notebook(self.resultFrame, height= 300, width = 600)
+                
     def loadBaseDirectory(self):
-        self.baseDir.set(tkFileDialog.askdirectory(parent=self.master, initialdir=os.getcwd(), title='Please select a directory stored log files'))
+        dirname = tkFileDialog.askdirectory(parent=self.master, initialdir=os.getcwd(), title='Please select a directory stored log files')
+        if len(dirname) > 0:
+            print ("Log File Base Directory %s " % dirname)
+            self.baseDir.set(dirname)
+        else:
+            print ("Log File Base Directory not selected.")
+        
+    def loadWatchList(self):
+        watchListFile = tkFileDialog.askopenfile(parent=self.master, mode='rb', title='Choose a file contains watch list')
+        if watchListFile != None:
+            self.watchListName.set(watchListFile.name)
+            print ("Watch List file selected: %s" % watchListFile.name)
+            with open(watchListFile.name) as f:
+                self.watchList = f.readlines()
+            self.watchList = [x.strip() for x in self.watchList]
+        else:
+            print ("No watch list file selected.")    
+    
+    def onClickStart(self):
+        # Stop current running job
+        # Start new job list
+        self.startAll()
+    
+    def onClickStop(self):
+        self.stopAll()
+        
+    def onClickExit(self):
+        print ("Exit...")
+        sys.exit()
+   
+    def stopAll(self):
+        self.running = False
+        for job in self.jobs:
+            job.terminate()
+            job.join()
+        self.jobs = []
+    
+    def parseLogs(self):
+        self.running = True
+        
+        # Clean Result Frame
+        self.cleanResult()
+                
+        # Get directory list
+        targetList = [""] + [name for name in os.listdir(self.baseDir.get())
+            if os.path.isdir(os.path.join(self.baseDir.get(), name))]
+        targetList = [os.path.join(self.baseDir.get(), basename) for basename in targetList]
+        
+        for path in targetList:
+#             p = multiprocessing.Process(target=Log, args=(self.watchList,path))
+#             p.start()
+#             pass
+            logTab = LogParser.LogParser(path=path, watchList=self.watchList, tabControl=self.tabControl)
+            self.jobs.append(logTab)
+            logTab.start()    
+    
+    def startAll(self):
+        if self.autoRefresh.get() == 0:
+            self.parseLogs()
+        else:
+            self.parseLogs()
+            self.resultFrame.after(2000, self.startParse)        
+    
+        
+    def cleanResult(self):
+        # Wait all threads finish
+        for i in self.jobs:
+            i.join(60) 
+            self.tabControl.forget(i.tab)
+        
+        self.jobs=[]
+#         
+#         if self.autoRefresh.get() == 0:
+#             self.parseLogs(tabControl)
+#         else:
+#             self.parseLogs(tabControl)
+#             resultFrame.after(5000, self.startParse)
+#             
+#     def parseLogs(self, tabControl):  
+#         
+#         # Get directory list
+#         dirList = [""] + [name for name in os.listdir(self.baseDir.get())
+#             if os.path.isdir(os.path.join(self.baseDir.get(), name))]
+#         targetList = [path for path in dirList if not self.findTargetDir(path) == None]
+#         targetList = [os.path.join(self.baseDir.get(), basename) for basename in targetList]
+#                 
+#         # Create tabs for each directories
+#         self.jobs = []
+#         for path in targetList:
+#             tab = self.createTab(tabControl, path)
+#             p = _LogParser.LogParserProcess(path, self.watchList, tab)
+#             self.jobs.append(p)
+#             p.start()
+#         
+#         for i in self.jobs:
+#             i.join()
+#     
+#     def createTab(self, tabControl, path):
+#         tab = ttk.Frame(tabControl)
+#         tabName = ntpath.basename(path)
+#         tabControl.add(tab, text=tabName)
+#         tabControl.pack(expand=1, fill=BOTH)
+#         return tab
+    
+#     def findTargetDir(self, path):
+#         fullPath = os.path.join(self.baseDir.get(), path)
+#         files = [f for f in os.listdir(fullPath) if re.match('.*.log', f)]
+#         if files == []:
+#             return None
+#         else:
+#             return path   
+
+# def createNewTab(tabControl):
+
+    
+def Log(watchList, path):    
+    listBox = LogParser.LogParser(watchList, path)
 
 
 if __name__ == '__main__':
